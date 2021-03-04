64ba041a7a0bbbdfa701ee1af7bb6bed695c931c
Yzadok <yaniv@zadok.ws>
2021-03-04 22:56:26 +0200

    saved to upload to github

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            lesson040321.Main1();
+        }
+    }
+}
---
+++ lesson040321.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lessons
+{
+    class lesson040321
+    {
+        public static void Main1()
+        {
+            Console.WriteLine("Enter First number:");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second number:");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int z = Add(a, b);
+            Console.WriteLine($"{a} + {b} = {z}" + z.ToString());
+
+
+
+            Console.ReadLine();
+
+
+        }
+
+        static int Add(int x, int y)
+        {
+
+            return x + y;
+
+
+
+        }
+    }
+}
